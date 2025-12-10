51851b4657dd91dc7a9f037053a0a7647dbb748c
Allard Veneman <allard@b2design.nl>
2025-12-10 10:51:15 +0100

    Added bootstrap 5

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,20 @@ import HelloWorld from "./components/HelloWorld.vue";
 
 <template>
   <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
+    <div class="row align-items-center">
+      <div class="col-6">
+        <img
+          alt="Vue logo"
+          class="logo"
+          src="./assets/logo.svg"
+          width="125"
+          height="125"
+        />
+      </div>
+      <div class="col-6">
+        test
+      </div>
+    </div>
 
     <div class="wrapper">
       <HelloWorld msg="Scibidie" />
